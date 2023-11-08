ef3b8c24f2750e0b547284403304f5ac31f6e6a4
Andre Bandarra <andreban@gmail.com>
2023-11-08 19:03:52 +0000

    Renders tiles

--- src/index.ts
+++ src/index.ts
@@ -5,6 +5,10 @@ const BOARD_WIDTH = 20;
 const BOARD_HEIGHT = 20;
 const TILE_IDS = [0, 1, 2, 3, 5, 6, 7, 8, 9, 10, 11];
 const TILE_SPRITESHEET_URL = new URL('pipe-tileset.png', import.meta.url);
+const SPRITESHEET_INNER_PADDING = 2;
+const SPRITESHEET_NUM_COLS = 6;
+const ALPHA_OPAQUE = 1.0;
+const CANVAS_STYLE_BLACK = '#000000'
 
 const outputCanvas = document.querySelector('#output-canvas') as HTMLCanvasElement;
 const ctx = outputCanvas.getContext('2d')!;
@@ -37,17 +41,19 @@ for (let row = 0; row < BOARD_HEIGHT; row++) {
     }
 }
 
-let image = loadImage(TILE_SPRITESHEET_URL)
+loadImage(TILE_SPRITESHEET_URL)
     .then((img) => {
         // Render Tiles
         for (let tile of tiles) {
             let x = tile.x * TILE_SIZE;
             let y = tile.y * TILE_SIZE;
             for (let index of tile.options) {
-                let row = index / 6
-                let col = index  % 6;
-                ctx.drawImage(img, 0, 0, 32, 32, 0, 0, 32, 32);
-                ctx.strokeStyle = '#0';
+                const srcY = Math.floor(index / SPRITESHEET_NUM_COLS) * (TILE_SIZE + SPRITESHEET_INNER_PADDING)
+                const srcX = index % SPRITESHEET_NUM_COLS * (TILE_SIZE + SPRITESHEET_INNER_PADDING);
+                ctx.globalAlpha = ALPHA_OPAQUE / tile.options.length;
+                ctx.drawImage(img, srcX, srcY, TILE_SIZE, TILE_SIZE, x, y, TILE_SIZE, TILE_SIZE);
+                ctx.globalAlpha = ALPHA_OPAQUE
+                ctx.strokeStyle = CANVAS_STYLE_BLACK;
                 ctx.strokeRect(x, y, TILE_SIZE, TILE_SIZE);
             }
         }
